d659336dbc14fb1668f56765b7e3ee41433d790a
LouisGautierTransway <louis.gautier@outlook.fr>
2018-01-15 10:40:50 +0100

    new users model

--- models/users.js
+++ models/users.js
@@ -4,15 +4,30 @@ mongoose.connect('mongodb://localhost:27017/godDatabase', { useMongoClient: true
 mongoose.Promise = global.Promise;
 
 // model
-var users = new mongoose.Schema({
-  pseudo: { type: String }, //match: /^[a-zA-Z0-9-_]+$/
-  password: { type: String }, //match: /^[a-zA-Z0-9-_]+$/
-  email: { type: String }, //match: /^[a-zA-Z0-9-_]+$/
-  signUpDate: { type: Date, default: Date.now },
-  gold: { type: Number },
-  totalGold: { type: Number }
-
+let equippedGear = new mongoose.Schema({
+    propeller : { type : String }, //gear_id
+    engine : { type : String }, //gear_id
+    shield : { type : String }, //gear_id
+    weapons : [String] //gear_id
 });
+let drones = new mongoose.Schema({
+    drone : { type : String }, //drone_id
+    isCurrent : { trype : boolean},
+    equippedGears : { type : equippedGear }
+});
+
+let users = new mongoose.Schema({
+	name : { type : String },
+	password : { type : String },
+    email: { type: String },
+    signUpDate: { type: Date, default: Date.now },
+    gold: { type: Number },
+    totalGold: { type: Number },
+	boughtGears : [String], //gear_id
+	drones : [drones]
+});
+
+
 var User = mongoose.model('users',users);
 
 module.exports = {
